d10986918ca9ebed84bbcd4d73f0289021554d4d
Jonathan Garbee <jonathan@garbee.me>
2020-09-12 16:57:51 -0400

    git and fish config tracked.

--- install-scripts/macos.sh
+++ install-scripts/macos.sh
@@ -1,6 +1,12 @@
 #!/bin/zsh
+
 # Make ZSH not care about end of line comments when running the script
 setopt interactive_comments
+
+# Silence any MoTD or "last login" message when starting a shell
+if [ ! -f $HOME/.hushlogin ]; then
+    touch $HOME/.hushlogin
+fi
 
 ask() {
     # https://djm.me/ask
@@ -39,6 +45,17 @@ ask() {
     done
 }
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the install script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+if ask "Do you want to clear all dock icons?"; then
+    defaults write com.apple.dock persistent-apps -array
+    killAll Dock
+fi
+
 casksToInstall=(
     "1password-beta"
     "font-iosevka"
@@ -50,7 +67,6 @@ casksToInstall=(
     "dash"
     "kaleidoscope"
     "dash"
-    "fork"
     "iterm2"
     "alfred"
     "discord"
@@ -58,26 +74,31 @@ casksToInstall=(
     "airparrot"
     "forklift"
     "mission-control-plus"
-    "itsycal"
     "fsmonitor"
     "soundsource"
     "focusatwill"
+    "sony-ps4-remote-play"
+    "typora"
+    "geekbench"
+    "notion"
 )
 
-if ask "Do you want to install Parallels? (You will need to be present to provide the password for install)"; then
+if ask "Do you want to install App Tamer?"; then
+    casksToInstall+=("app-tamer")
+fi
+
+if ask "Do you want to install Parallels?"; then
     casksToInstall+=("parallels")
 fi
 
-if ask "Do you want to install Steam?"; then
+if ask "Do you want to install game libraries (steam gog)?"; then
     casksToInstall+=("steam")
+    casksToInstall+=("gog-galaxy")
 fi
 
 if ask "Do you want to install Open Broadcast Studio?"; then
     casksToInstall+=("obs")
 fi
-
-vared -p 'What global name would you like to use for git?: ' -c gitName
-vared -p 'What global email would you like to use for git?: ' -c gitEmail
 
 # Make temp folder for holding some files
 tempDir=$(mktemp -d)
@@ -118,10 +139,11 @@ mkdir -p $HOME/Library/Application Support/Google/Chrome/
 ## Misc
 mkdir -p $HOME/Code
 mkdir -p $HOME/bin
+mkdir -p $HOME/.ssh
 
 # Install Homebrew
-if ! which brew &> /dev/null; then
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if ! command -v brew &> /dev/null; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
 # Install brewed software
@@ -130,14 +152,25 @@ echo "Installing homebrew software"
 brew tap homebrew/cask
 brew tap homebrew/cask-versions
 brew tap homebrew/cask-fonts
+brew tap homebrew/cask-drivers
 
 formulaeToInstall=(
 	"node"
-	"yarn"
 	"coreutils"
 	"git"
 	"mas"
 	"fish"
+    "git-flow"
+    "git-delta"
+    "git-quick-stats"
+    "gnupg"
+    "httpie"
+    "pstree"
+    "tldr"
+    "bat"
+    "exa"
+    "jq"
+    "1password-cli"
 )
 
 for target in $formulaeToInstall; do
@@ -190,30 +223,106 @@ echo "Configuring Fish Shell"
 echo $(which fish) | sudo tee -a /etc/shells
 chsh -s $(which fish)
 
-### Itsycal
-echo "Configuring Itsycal"
-defaults write -app Itsycal HideIcon -bool true
-defaults write -app Itsycal SizePreference -bool true
-defaults write -app Itsycal ClockFormat "hh:mm:ss a - EEE, MMM dd yyyy"
-# attempt to set to auto-launch on login. Doesn"t work. To debug.
-# defaults write loginwindow AutoLaunchedApplicationDictionary -array-add "{ "Path" = "/Applications/Itsycal.app"; "Hide" = 0; }"
-
 ### iTerm
 echo "Configuring iTerm"
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles/iterm2"
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 
 ### Setup CotEdtior
-echo "Configuring CotEditor"
 defaults write -app CotEditor fontName "Iosevka"
 defaults write -app CotEditor fontSize 14
 defaults write -app CotEditor lineHeight 1.5
 defaults write -app CotEditor highlightCurrentLine -bool true
 
+# Disable automatic capitalization as it"s annoying when typing code
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+
+# Disable smart dashes as they"re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+# Disable automatic period substitution as it"s annoying when typing code
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+
+# Disable smart quotes as they"re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
+# Disable auto-correct
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+# Enable full keyboard access for all controls
+# (e.g. enable Tab in modal dialogs)
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+# Finder: show hidden files by default
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# Avoid creating .DS_Store files on network or USB volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Enable AirDrop over Ethernet and on unsupported Macs running Lion
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
+
+# Show the ~/Library folder
+chflags nohidden ~/Library && xattr -d com.apple.FinderInfo ~/Library
+
+# Show the /Volumes folder
+sudo chflags nohidden /Volumes
+
+# Minimize windows into their application"s icon
+defaults write com.apple.dock minimize-to-application -bool true
+
+# Set the icon size of Dock items to 36 pixels
+defaults write com.apple.dock tilesize -int 36
+
+# Don"t automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false
+
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
+# Don"t show recent applications in Dock
+defaults write com.apple.dock show-recents -bool false
+
+# Enable Safari"s debug menu
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+
+# Enable the Develop menu and the Web Inspector in Safari
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
+
+# Prevent Time Machine from prompting to use new hard drives as backup volume
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+# Check for software updates daily, not just once per week
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
+# Download newly available updates in background
+defaults write com.apple.SoftwareUpdate AutomaticDownload -int 1
+
+# Turn on app auto-update
+defaults write com.apple.commerce AutoUpdate -bool true
+
+# Set Forklift as default file viewer where possible
+defaults write -g NSFileViewer -string com.binarynights.forklift-setapp
+defaults write com.apple.LaunchServices/com.apple.launchservices.secure LSHandlers -array-add '{LSHandlerContentType="public.folder";LSHandlerRoleAll="com.binarynights.forklift-setapp";}'
+
 ### Setup Depot Tools
 echo "Installing depot tools"
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git $HOME/bin/depot_tools
 
+
+if [ ! -f $HOME/.iterm2_shell_integration.fish ]; then
+    http --output $HOME/.iterm2_shell_integration.fish https://iterm2.com/shell_integration/fish
+fi
+
+COMPLETION_DIR=$HOME/.config/fish/completions
+mkdir -p $COMPLETION_DIR
+
+if [ ! -f $COMPLETION_DIR/git-flow.fish ]; then
+    http --output $COMPLETION_DIR/git-flow.fish https://raw.githubusercontent.com/bobthecow/git-flow-completion/master/git.fish
+fi
 
 # Cleanup
 echo "Cleaning up"
