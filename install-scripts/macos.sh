ac8120a2a1c7c6f05163c3388c608964714ace17
Jonathan Garbee <jonathan@garbee.me>
2025-10-04 13:58:08 -0400

    updates...

--- install-scripts/macos.sh
+++ install-scripts/macos.sh
@@ -69,54 +69,9 @@ __dock_item() {
         '</dict></dict></dict>'
 }
 
-if ask "Do you want to clear all dock icons?" N; then
-    defaults write com.apple.dock persistent-apps -array
-    defaults write com.apple.dock persistent-apps -array-add "$(__dock_item /System/Applications/Launchpad.app)"
-    defaults write com.apple.dock persistent-apps -array-add "$(__dock_item /Applications/Safari.app)"
-    killAll Dock
-fi
-
 if ask "Do you want to install Rosetta 2?" Y; then
     softwareupdate --install-rosetta
 fi
-
-casksToInstall=(
-    "font-iosevka"
-    "font-iosevka-slab"
-    "font-iosevka-nerd-font"
-    "font-input"
-    "font-victor-mono-nerd-font"
-    "font-lexend"
-    "font-lexend-exa"
-    "font-lexend-giga"
-    "font-lexend-mega"
-    "font-lexend-peta"
-    "font-lexend-tera"
-    "font-lexend-zetta"
-    "font-monaspace"
-    "font-monaspace-nerd-font"
-    "raycast"
-    "discord"
-    "fsmonitor"
-    "steam"
-    "contexts"
-    "elgato-stream-deck"
-    "screenflow"
-    "openemu"
-    "soundsource"
-    "audio-hijack"
-    "loopback"
-    "fission"
-    "visual-studio-code"
-    "signal"
-    "webstorm"
-    "goland"
-    "mps"
-    "datagrip"
-    "rapidapi"
-    "proxyman"
-    # Forklift?
-)
 
 # Make temp folder for holding some files
 tempDir=$(mktemp -d)
@@ -133,12 +88,109 @@ curl -L -o ~/Library/Colors/Nord.clr https://raw.githubusercontent.com/arcticice
 # Show seconds in clock
 defaults write com.apple.menuextra.clock ShowSeconds -bool true
 defaults write com.apple.menuextra.clock DateFormat "EEE MMM d  h:mm:ss a"
-# Show the path bar
+
+# Disable automatic capitalization as it"s annoying when typing code
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+
+# Disable smart dashes as they"re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+# Disable automatic period substitution as it"s annoying when typing code
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+
+# Disable smart quotes as they"re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
+# Disable auto-correct
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+# Enable full keyboard access for all controls
+# (e.g. enable Tab in modal dialogs)
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+# Avoid creating .DS_Store files on network or USB volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Enable AirDrop over Ethernet and on unsupported Macs running Lion
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
+
+# Set the icon size of Dock items to 36 pixels
+defaults write com.apple.dock tilesize -int 36
+
+# Don"t automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false
+
+# Prevent Time Machine from prompting to use new hard drives as backup volume
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+# Check for software updates daily, not just once per week
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
+# Download newly available updates in background
+defaults write com.apple.SoftwareUpdate AutomaticDownload -int 1
+
+# Turn on app auto-update
+defaults write com.apple.commerce AutoUpdate -bool true
+
+# Change screenshot save location
+screenhot_location="~/Pictures/Screenshots"
+defaults write com.apple.iphonesimulator "ScreenShotSaveLocation" -string "$screenhot_location"
+defaults write com.apple.screencapture "location" -string "$screenhot_location"
+defaults write com.apple.screencapture "include-date" -bool "false"
+killall SystemUIServer
+
+# Dock settings
+defaults write com.apple.dock minimize-to-application -bool true
+defaults write com.apple.dock "mineffect" -string "genie"
+defaults write com.apple.dock "show-recents" -bool "false"
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock magnification -bool true
+
+set -u
+
+PLIST="$HOME/Library/Preferences/com.apple.dock.plist"
+BUDDY="/usr/libexec/PlistBuddy"
+
+defaults read com.apple.dock >/dev/null 2>&1 || true
+
+if $BUDDY -c "Print persistent-apps" "$PLIST" >/dev/null 2>&1; then
+  COUNT=$($BUDDY -c "Print persistent-apps" "$PLIST" 2>/dev/null | grep -c "Dict")
+else
+  COUNT=0
+fi
+
+INDEX=0
+while [ $INDEX -lt $COUNT ]; do
+  LABEL=$($BUDDY -c "Print persistent-apps:$INDEX:tile-data:file-label" "$PLIST" 2>/dev/null || echo "")
+
+  if [[ "$LABEL" != "Safari" ]]; then
+    $BUDDY -c "Delete persistent-apps:$INDEX" "$PLIST"
+    COUNT=$((COUNT-1))
+    INDEX=$((INDEX-1))
+  fi
+
+  INDEX=$((INDEX+1))
+done
+
+killall Dock
+
+# Finder prefs
+defaults write com.apple.finder "ShowHardDrivesOnDesktop" -bool "false"
+defaults write com.apple.finder "ShowExternalHardDrivesOnDesktop" -bool "false"
+defaults write com.apple.finder "ShowRemovableMediaOnDesktop" -bool "false"
+defaults write com.apple.finder "ShowMountedServersOnDesktop" -bool "false"
+defaults write com.apple.finder "FXEnableExtensionChangeWarning" -bool "false"
+defaults write com.apple.finder "FXDefaultSearchScope" -string "SCcf"
+defaults write com.apple.finder "_FXSortFoldersFirst" -bool "true"
+defaults write com.apple.finder "FXPreferredViewStyle" -string "Nlsv"
+defaults write com.apple.finder "AppleShowAllFiles" -bool "true"
+defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true"
 defaults write com.apple.finder ShowPathbar -bool true
-# Show the status bar
 defaults write com.apple.finder ShowStatusBar -bool true
-# Show user library folder
-chflags nohidden ~/Library
+chflags nohidden ~/Library && xattr -d com.apple.FinderInfo ~/Library
+sudo chflags nohidden /Volumes
+killall Finder
 
 # Enable developer options in Safari
 defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
@@ -182,13 +234,23 @@ fi
 brew install --cask "1password"
 brew install --cask "1password-cli"
 
-# Install brewed software
-echo "Tapping"
-brew tap homebrew/cask-fonts
+if ask "Install Docker?" Y; then
+    echo "Downloading Docker"
+    dockerImagePath="$tempDir/Docker.dmg"
+    curl --progress-bar -L -o "$dockerImagePath" https://desktop.docker.com/mac/main/arm64/Docker.dmg
+    echo "Mounting Docker Installer"
+    sudo hdiutil attach "$dockerImagePath" -quiet
+    echo "Installing Docker"
+    sudo /Volumes/Docker/Docker.app/Contents/MacOS/install --accept-license --user=$USER
+    echo "Unmounting installer"
+    sudo hdiutil detach /Volumes/Docker
+    rm "$dockerImagePath"
+fi
+
 echo "Installing homebrew software"
 
 formulaeToInstall=(
-    "starship"
+    "fish"
     "node"
     "coreutils"
     "git"
@@ -208,34 +270,37 @@ formulaeToInstall=(
     "prettyping"
     "the_silver_searcher"
     "fzf"
-    "blacktop/tap/lporg"
+    "harper"
+    "font-iosevka"
+    "font-iosevka-slab"
+    "font-iosevka-nerd-font"
+    "font-input"
+    "font-victor-mono-nerd-font"
+    "font-lexend"
+    "font-lexend-exa"
+    "font-lexend-giga"
+    "font-lexend-mega"
+    "font-lexend-peta"
+    "font-lexend-tera"
+    "font-lexend-zetta"
+    "font-monaspace"
+    "font-monaspace-nerd-font"
+    "discord"
+    "visual-studio-code"
+    "rapidapi"
+    "proxyman"
+    "macwhisper"
+    "chatgpt"
+    "iterm2"
+    "latest"
 )
 
 for target in $formulaeToInstall; do
-    brew list $target &>/dev/null || brew install $target
+    if ! brew list $target &>/dev/null; then
+        echo "Installing $target"
+        brew install --quiet $target
+    fi
 done
-
-for target in $casksToInstall; do
-    brew list $target &>/dev/null || brew install --cask $target
-done
-
-echo "Downloading Docker"
-dockerImagePath="$tempDir/Docker.dmg"
-curl --progress-bar -L -o "$dockerImagePath" https://desktop.docker.com/mac/main/arm64/Docker.dmg
-echo "Mounting Docker Installer"
-sudo hdiutil attach "$dockerImagePath" -quiet
-echo "Installing Docker"
-sudo /Volumes/Docker/Docker.app/Contents/MacOS/install --accept-license --user=$USER
-echo "Unmounting installer"
-sudo hdiutil detach /Volumes/Docker
-rm "$dockerImagePath"
-
-if ask "Are you logged into 1Password with CLI integration active?" Y; then
-    # Place FSMonitor License in order to activate it.
-    op read "op://Software Licenses/FSMonitor - license.fsmonitorlicense/license.fsmonitorlicense" --out-file=/Users/Shared/FSMonitor/license.fsmonitorlicensefi
-    # Login to docker
-    op read "op://Personal/Docker Hub/Access Tokens/Personal Laptop" | docker login -u $(op read "op://Personal/Docker Hub/username") --password-stdin
-fi
 
 # Install AppStore Content
 
@@ -248,18 +313,16 @@ appStoreApps+=("1533805339") # Keepa - Price Tracker
 appStoreApps+=("1482490089") # Tampermonkey
 appStoreApps+=("1569813296") # 1Password for Safari
 appStoreApps+=("6449850851") # Privacy
-appStoreApps+=("1534275760") # LanguageTool
 appStoreApps+=("1622835804") # Kagi
 appStoreApps+=("1615431236") # Bonjourr Startpage
-appStoreApps+=("6449850851") # Privacy.com for Safari
 
 # Media
-appStoreApps+=("1484348796") # Endel
+appStoreApps+=("1346247457") # Endel
+appStoreApps+=("1436994560") # Portal
 
 # Utilities
 appStoreApps+=("1508732804") # Soulver 3
 appStoreApps+=("1452453066") # Hidden Bar
-appStoreApps+=("441258766")  # Magnet
 appStoreApps+=("470158793")  # Keka
 appStoreApps+=("411643860")  # DaisyDisk
 appStoreApps+=("1588708173") # Elsewhen
@@ -269,39 +332,24 @@ appStoreApps+=("403504866")  # PCalc
 appStoreApps+=("1559348223") # Power Plist Editor
 appStoreApps+=("499768540")  # Power JSON Editor
 appStoreApps+=("1565766176") # Power YAML Editor
-appStoreApps+=("6444068649") # Codepoint
 appStoreApps+=("1024640650") # CotEditor
+appStoreApps+=("1569680330") # Rsyncinator
+appStoreApps+=("6446933691") # Postico 2
 
 # Productivity
-appStoreApps+=("1515324201") # Planny
 appStoreApps+=("409203825")  # Numbers
 appStoreApps+=("409201541")  # Pages
 appStoreApps+=("409183694")  # Keynote
 appStoreApps+=("408981434")  # iMovie
-appStoreApps+=("1616831348") # Affinity Designer 2
-appStoreApps+=("1606941598") # Affinity Publisher 2
-appStoreApps+=("1616822987") # Affinity Photo 2
 appStoreApps+=("890031187")  # Marked 2
-appStoreApps+=("1527036273") # Taio
-appStoreApps+=("1441250616") # Clean Email
-
-# DevTools
-appStoreApps+=("1578175415") # Codeface
-appStoreApps+=("1569680330") # Rsyncinator
-# Clobbr?
-# Silenz?
+appStoreApps+=("1583719331") # Lunatask
+appStoreApps+=("1663047912") # Screens 5
+appStoreApps+=("1522267256") # Shareful
+appStoreApps+=("975937182") # Fantastical
 
 for appId in $appStoreApps; do
     mas install "$appId"
 done
-
-hostsPrefPath=/Library/PreferencePanes/Hosts.prefPane
-
-if [ ! -d $hostsPrefPath ]; then
-    curl -O https://www.dirk-froehling.de/resources/Software/Hosts-PrefPane-1.4.5.pkg
-    sudo installer -pkg Hosts-PrefPane-1.4.5.pkg -target /
-    rm Hosts-PrefPane-1.4.5.pkg
-fi
 
 ## Configurations
 
@@ -321,103 +369,11 @@ if [ ! -f "$HOME/.zshrc" ]; then
     ln -s "$HOME/.dotfiles/config/zshrc" "$HOME/.zshrc"
 fi
 
-# Disable automatic capitalization as it"s annoying when typing code
-defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-# Disable smart dashes as they"re annoying when typing code
-defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
-
-# Disable automatic period substitution as it"s annoying when typing code
-defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
-
-# Disable smart quotes as they"re annoying when typing code
-defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
-
-# Disable auto-correct
-defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
-
-# Enable full keyboard access for all controls
-# (e.g. enable Tab in modal dialogs)
-defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
-
-# Finder: show hidden files by default
-defaults write com.apple.finder AppleShowAllFiles -bool true
-
-# Avoid creating .DS_Store files on network or USB volumes
-defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
-
-# Enable AirDrop over Ethernet and on unsupported Macs running Lion
-defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
-
-# Show the ~/Library folder
-chflags nohidden ~/Library && xattr -d com.apple.FinderInfo ~/Library
-
-# Show the /Volumes folder
-sudo chflags nohidden /Volumes
-
-# Minimize windows into their application"s icon
-defaults write com.apple.dock minimize-to-application -bool true
-
-# Set the icon size of Dock items to 36 pixels
-defaults write com.apple.dock tilesize -int 36
-
-# Don"t automatically rearrange Spaces based on most recent use
-defaults write com.apple.dock mru-spaces -bool false
-
-# Automatically hide and show the Dock
-defaults write com.apple.dock autohide -bool true
-
-# Don"t show recent applications in Dock
-defaults write com.apple.dock show-recents -bool false
-
-# Minimize apps to app icon
-defaults write com.apple.dock minimize-to-application -bool true
-
-# Magnify apps on hover
-defaults write com.apple.dock magnification -bool true
-
-# Enable Safari's debug menu
-defaults write .GlobalPreferences WebkitDeveloperExtras -bool true
-
-# Enable the Develop menu and the Web Inspector in Safari
-defaults write com.apple.Safari IncludeDevelopMenu -bool true
-defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
-defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
-
-# Prevent Time Machine from prompting to use new hard drives as backup volume
-defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
-
-# Check for software updates daily, not just once per week
-defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
-
-# Download newly available updates in background
-defaults write com.apple.SoftwareUpdate AutomaticDownload -int 1
-
-# Turn on app auto-update
-defaults write com.apple.commerce AutoUpdate -bool true
-
-## GAMES
-# appStoreGames+=("564196178") # The Witcher 2
-
-appStoreGames=()
-
-appStoreGames+=("1626348601") # Inscryption
-appStoreGames+=("1671189271") # Lumencraft
-appStoreGames+=("1471313227") # The Pillar
-appStoreGames+=("1406390771") # Old Man's Journey
-appStoreGames+=("1203245278") # Kenshō
-appStoreGames+=("408054320")  # Osmos
-appStoreGames+=("416608891")  # Knights of the Old Republic
-appStoreGames+=("960778634")  # Star Wars®: Knights of the Old Republic™ II
-appStoreGames+=("509542875")  # The Witcher: Enhanced Edition
-appStoreGames+=("564196178")  # The Witcher 2
-appStoreGames+=("1087338873") # Divinity - Original Sin Enhanced Edition
-appStoreGames+=("1441532941") # Divinity - Original Sin 2
-
-for appId in $appStoreGames; do
-    mas install "$appId"
-done
+which fish | sudo tee -a /etc/shells
+chsh -s "$(which fish)" "$USER"
 
 cd $HOME/.dotfiles
 git remote set-url origin git@github.com:Garbee/dotfiles.git
@@ -426,3 +382,33 @@ cd $HOME
 # Cleanup
 echo "Cleaning up"
 rm -rf "$tempDir"
+
+# Manual Tasks
+cat <<<EOF
+Setup is now complete.
+There are a few manual tasks to finish so things are fully functional.
+
+First, go into the Privacy and Security system preferences.
+The following should be granted permissions:
+
+* Full Disk access
+   * Iterm2
+   * Terminal
+   * Visual Studio Code
+   * Zed
+* Accessibility
+   * 1Password
+   * MacWhisper
+* App Management
+   * Iterm2
+   * Terminal
+   * Latest
+* Screen & System Audio Recording
+   * MacWhisper
+   * Zoom
+   * Slack
+EOF
+
+if ask "Do you want to open the Privacy and Security system preferences now?"; then
+    open "x-apple.systempreferences:com.apple.preference.security?Privacy_All"
+fi
